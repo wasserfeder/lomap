cc243f406746011b69b9e37df91204bb1de324eb
wasserfeder <ptheory@gmail.com>
2016-11-02 10:19:22 -0400

    Changed to spaces-based indetation and corrected logger.info calls.

--- classes/timer.py
+++ classes/timer.py
@@ -23,27 +23,29 @@ __author__ = 'Alphan Ulusoy (alphan@bu.edu)'
 logger = logging.getLogger(__name__)
 #logger.addHandler(logging.NullHandler())
 
+
 class Timer:
-	"""
-	LOMAP timer class.
+    """
+    LOMAP timer class.
 
-	Examples:
-	---------
-	>>> with lomap.Timer():
-	>>>	 time.sleep(0.1)
-	Operation took 100 ms.
+    Examples:
+    ---------
+    >>> with lomap.Timer():
+    >>>     time.sleep(0.1)
+    Operation took 100 ms.
 
-	>>> with lomap.Timer('Taking product'):
-	>>>	 time.sleep(0.1)
-	Taking product took 100 ms.
-	"""
-	def __enter__(self):
-		logger.info('%s started.' % self.op_name)
-		self.start = time.time()
-	def __init__(self, op_name=None):
-		if op_name is not None:
-			self.op_name = op_name
-		else:
-			self.op_name = "Operation"
-	def __exit__(self, *args): 
-		logger.info('%s took %0.3f ms.' % (self.op_name, (time.time() - self.start)*1000))
+    >>> with lomap.Timer('Taking product'):
+    >>>     time.sleep(0.1)
+    Taking product took 100 ms.
+    """
+    def __enter__(self):
+        logger.info('%s started.', self.op_name)
+        self.start = time.time()
+    def __init__(self, op_name=None):
+        if op_name is not None:
+            self.op_name = op_name
+        else:
+            self.op_name = "Operation"
+    def __exit__(self, *args): 
+        logger.info('%s took %0.3f ms.', self.op_name,
+                    (time.time() - self.start)*1000)
