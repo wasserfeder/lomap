28a942ecb669b250ddd3a80b72dbd397fdb80e66
Eleni Karyofylli <65410718+Elenikary@users.noreply.github.com>
2020-06-20 15:54:06 -0400

    Update timer.py

--- lomap/classes/timer.py
+++ lomap/classes/timer.py
@@ -1,3 +1,6 @@
+#! /usr/bin/python
+
+from __future__ import print_function
 # Copyright (C) 2012-2015, Alphan Ulusoy (alphan@bu.edu)
 # 
 # This program is free software; you can redistribute it and/or modify
@@ -13,7 +16,7 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
+#from builtins import object
 import time
 import logging
 
@@ -24,7 +27,7 @@ logger = logging.getLogger(__name__)
 #logger.addHandler(logging.NullHandler())
 
 
-class Timer:
+class Timer(object):
     """
     LOMAP timer class.
 
