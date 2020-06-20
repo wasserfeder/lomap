3ac9b2c98b6b965e20f8f681ade136b4cbcfcfd5
Eleni Karyofylli <65410718+Elenikary@users.noreply.github.com>
2020-06-20 15:53:06 -0400

    Update timer.py

--- lomap/classes/timer.py
+++ lomap/classes/timer.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python
+
 from __future__ import print_function
 # Copyright (C) 2012-2015, Alphan Ulusoy (alphan@bu.edu)
 # 
@@ -14,7 +16,7 @@ from __future__ import print_function
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-from builtins import object
+#from builtins import object
 import time
 import logging
 
