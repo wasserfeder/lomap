aa7adcc731a65e0928e99af6acd437cfc499dfac
Eleni Karyofylli <65410718+Elenikary@users.noreply.github.com>
2020-06-20 15:43:13 -0400

    Update interval.py

--- lomap/classes/interval.py
+++ lomap/classes/interval.py
@@ -1,3 +1,6 @@
+#! /usr/bin/python
+
+from __future__ import print_function
 # Copyright (C) 2012-2015, Alphan Ulusoy (alphan@bu.edu)
 # 
 # This program is free software; you can redistribute it and/or modify
@@ -15,7 +18,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 """LOMAP interval class."""
-
+#from builtins import object
 import logging
 
 # Logger configuration
