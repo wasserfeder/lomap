3c67e404e433c2c81441265b0ebda838e2ac4b35
Eleni Karyofylli <65410718+Elenikary@users.noreply.github.com>
2020-06-20 15:48:08 -0400

    Update model.py

--- lomap/classes/model.py
+++ lomap/classes/model.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python
+
 from __future__ import print_function
 # Copyright (C) 2012-2015, Alphan Ulusoy (alphan@bu.edu)
 #               2015-2017, Cristian-Ioan Vasile (cvasile@mit.edu)
@@ -15,7 +17,7 @@ from __future__ import print_function
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-from builtins import object
+#from builtins import object
 import networkx as nx
 
 # TODO: always use safe load
